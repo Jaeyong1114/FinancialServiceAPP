0a6886b50309d28e6d0052cf8cfeda3f273f92fe
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-10-09 16:21:48 +0900

    phone Authentication UI

--- app/src/main/java/com/example/financialserviceapp/IdentityInputActivity.kt
+++ app/src/main/java/com/example/financialserviceapp/IdentityInputActivity.kt
@@ -1,8 +1,15 @@
 package com.example.financialserviceapp
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import com.example.financialserviceapp.databinding.ActivityIdentityInputBinding
+import com.example.financialserviceapp.util.ViewUtil.hideKeyboard
+import com.example.financialserviceapp.util.ViewUtil.setOnEditorActionListener
+import com.example.financialserviceapp.util.ViewUtil.showKeyboard
+import com.example.financialserviceapp.util.ViewUtil.showKeyboardDelay
 
 class IdentityInputActivity : AppCompatActivity() {
 
@@ -13,6 +20,53 @@ class IdentityInputActivity : AppCompatActivity() {
         binding = ActivityIdentityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.view = this
+        initView()
+        binding.nameEdit.showKeyboardDelay()
+
+    }
+
+    private fun initView(){
+        with(binding){
+            nameEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT){
+                birthdayLayout.isVisible = true
+                birthdayEdit.showKeyboard()
+            }
+            birthdayEdit.doAfterTextChanged {
+                if( birthdayEdit.length() > 7){
+                    genderlayout.isVisible = true
+                    birthdayEdit.hideKeyboard()
+                }
+            }
+
+            genderChipGroup.setOnCheckedStateChangeListener{group, checkedIds ->
+                if(!telecomlayout.isVisible){
+                    telecomlayout.isVisible = true
+                }
+            }
+
+            telecomChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                if(!phoneLayout.isVisible){
+                    phoneLayout.isVisible = true
+                    phoneEdit.showKeyboard()
+                }
+
+            }
+            phoneEdit.doAfterTextChanged {
+                if(phoneEdit.length()>10){
+                    confirmButton.isVisible = true
+                    phoneEdit.hideKeyboard()
+                }
+            }
+
+            phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE){
+                if(phoneEdit.length()>9){
+                    confirmButton.isVisible = true
+                    phoneEdit.hideKeyboard()
+                }
+            }
+
+        }
+
 
     }
 
---
+++ app/src/main/java/com/example/financialserviceapp/util/ViewUtil.kt
@@ -0,0 +1,44 @@
+package com.example.financialserviceapp.util
+
+import android.content.Context
+import android.renderscript.ScriptGroup.Input
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import com.example.financialserviceapp.util.ViewUtil.showKeyboard
+
+object ViewUtil {
+
+    fun EditText.setOnEditorActionListener(action : Int, invoke: () -> Unit){
+        setOnEditorActionListener{_,actionId,_ ->
+            if(actionId == action){
+                invoke()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    fun EditText.showKeyboard(){
+        this.requestFocus()
+        val inputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as
+                InputMethodManager
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+
+    }
+
+    fun EditText.showKeyboardDelay(){
+        postDelayed({
+                    showKeyboard()
+        },200)
+    }
+
+    fun EditText.hideKeyboard(){
+        this.clearFocus()
+        val inputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as
+                InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken,0)
+
+    }
+
+}
